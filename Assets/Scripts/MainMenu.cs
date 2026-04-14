468791b678cf7b42990b6fa26f7d1a3852c14945
Luisdr25 <luisdr0596@hotmail.com>
2026-04-14 13:23:31 -0500

    Agregado el menu principal y assets de imagen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject optionsCreditos;
+
+    public void OpenOptionsPanel()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        optionsCreditos.SetActive(false);
+
+    }
+
+    public void OpenMainMenuPanel()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        optionsCreditos.SetActive(false);
+
+    }
+
+    public void AbrirCreditos()
+    {
+        mainMenu.SetActive(false);
+        optionsCreditos.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Prologo");
+    }
+}    
